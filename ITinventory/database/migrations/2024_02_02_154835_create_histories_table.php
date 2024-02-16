565d3960f4aab16788eb08b8f2a18ac01fd37063
FrL1902 <farrel.rasyad@binus.ac.id>
2024-02-16 17:08:41 +0700

    make migration for history

--- ITinventory/database/migrations/2024_02_02_154835_create_histories_table.php
+++ ITinventory/database/migrations/2024_02_02_154835_create_histories_table.php
@@ -15,6 +15,29 @@ class CreateHistoriesTable extends Migration
     {
         Schema::create('history', function (Blueprint $table) {
             $table->id();
+            $table->string('barang_id');
+            $table->string('barang');
+            $table->string('lokasi');
+            $table->string('by_user');
+            $table->string('status');
+            $table->string('model');
+            $table->string('kategori');
+            $table->string('Processor');
+            $table->string('RAM');
+            $table->string('GPU');
+            $table->string('Storage');
+            $table->string('OS');
+            $table->string('License');
+            $table->string('Monitor');
+            $table->string('Keyboard');
+            $table->string('Mouse');
+            $table->integer('stok')->default('0');
+            $table->string('SN'); #
+            $table->string('keterangan');
+            $table->string('gambar1');
+            $table->string('gambar2');
+            $table->string('assigned_user');
+            $table->boolean('is_pc')->default(false);
             $table->timestamps();
         });
     }
