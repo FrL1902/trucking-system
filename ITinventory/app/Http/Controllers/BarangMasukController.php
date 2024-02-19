36162c953b7c0bf94fdbbc8d2cf98c75f483bde9
FrL1902 <farrel.rasyad@binus.ac.id>
2024-02-19 15:24:35 +0700

    barang masuk udah bisa update

--- ITinventory/app/Http/Controllers/BarangMasukController.php
+++ ITinventory/app/Http/Controllers/BarangMasukController.php
@@ -47,21 +47,21 @@ class BarangMasukController extends Controller
                 'masuk_id' =>       $request->barang_masuk_id,
                 'model_id' =>       $request->model_id,
                 'location_id' =>    $request->lokasi_id,
-                'Processor' =>      ($request->is_pc) ? $request->processor : '-',
-                'RAM' =>            ($request->is_pc) ? $request->ram : '-',
-                'GPU' =>            ($request->is_pc) ? $request->gpu : '-',
-                'Storage' =>        ($request->is_pc) ? $request->storage : '-',
-                'OS' =>             ($request->is_pc) ? $request->operating_system : '-',
-                'License' =>        ($request->is_pc) ? $request->license : '-',
-                'Monitor' =>        ($request->is_pc) ? $request->monitor : '-',
-                'Keyboard' =>       ($request->is_pc) ? $request->keyboard : '-',
-                'Mouse' =>          ($request->is_pc) ? $request->mouse : '-',
-                'stok' =>           ($request->is_pc) ? 1 : $request->stok,
-                'SN' =>             ($request->SN) ? $request->SN : '-', #NI ngecek kalo ada SN ato ngga, kalo ngga ya diganti jadi -
+                'Processor' => ($request->is_pc) ? $request->processor : '-',
+                'RAM' => ($request->is_pc) ? $request->ram : '-',
+                'GPU' => ($request->is_pc) ? $request->gpu : '-',
+                'Storage' => ($request->is_pc) ? $request->storage : '-',
+                'OS' => ($request->is_pc) ? $request->operating_system : '-',
+                'License' => ($request->is_pc) ? $request->license : '-',
+                'Monitor' => ($request->is_pc) ? $request->monitor : '-',
+                'Keyboard' => ($request->is_pc) ? $request->keyboard : '-',
+                'Mouse' => ($request->is_pc) ? $request->mouse : '-',
+                'stok' => ($request->is_pc) ? 1 : $request->stok,
+                'SN' => ($request->SN) ? $request->SN : '-', #NI ngecek kalo ada SN ato ngga, kalo ngga ya diganti jadi -
                 'keterangan' =>     $request->keterangan,
                 'gambar1' =>        $imageName1,
                 'gambar2' =>        $imageName2,
-                'is_pc' =>          ($request->is_pc) ? $request->is_pc : false,
+                'is_pc' => ($request->is_pc) ? $request->is_pc : false,
                 'created_at' =>     Carbon::now(),
             ]);
 
@@ -77,7 +77,7 @@ class BarangMasukController extends Controller
             $lokasi = Location::find($request->lokasi_id);
             $lokasi = $lokasi->lokasi;          ####
 
-            $model_data = CategoryStock::find( $request->model_id);
+            $model_data = CategoryStock::find($request->model_id);
 
             $kategori = Category::find($model_data->category_id);
             $kategori = $kategori->kategori;    #####
@@ -105,13 +105,13 @@ class BarangMasukController extends Controller
                 'keterangan' =>     $request->keterangan,
                 'gambar1' =>        $imageName1,
                 'gambar2' =>        $imageName2,
-                'is_pc' =>          ($request->is_pc) ? $request->is_pc : false,
+                'is_pc' => ($request->is_pc) ? $request->is_pc : false,
                 'assigned_user' =>  '-',
                 'created_at' =>     Carbon::now(),
             ]);
         }
 
-        return redirect()->back();
+        return redirect()->back()->with('sukses_notif', 'Data Berhasil Di Tambahkan');
     }
 
     public function assign_barang_masuk(Request $request)
@@ -139,22 +139,22 @@ class BarangMasukController extends Controller
                 'keluar_id' =>      $request->keluar_id,
                 'model_id' =>       $barang_masuk->model_id,
                 'location_id' =>    $request->lokasi_id,
-                'Processor' =>      ($barang_masuk->is_pc) ? $barang_masuk->Processor : '-',
-                'RAM' =>            ($barang_masuk->is_pc) ? $barang_masuk->RAM : '-',
-                'GPU' =>            ($barang_masuk->is_pc) ? $barang_masuk->GPU : '-',
-                'Storage' =>        ($barang_masuk->is_pc) ? $barang_masuk->Storage : '-',
-                'OS' =>             ($barang_masuk->is_pc) ? $barang_masuk->OS : '-',
-                'License' =>        ($barang_masuk->is_pc) ? $barang_masuk->License : '-',
-                'Monitor' =>        ($barang_masuk->is_pc) ? $barang_masuk->Monitor : '-',
-                'Keyboard' =>       ($barang_masuk->is_pc) ? $barang_masuk->Keyboard : '-',
-                'Mouse' =>          ($barang_masuk->is_pc) ? $barang_masuk->Mouse : '-',
-                'stok' =>           ($barang_masuk->is_pc) ? 1 : $request->stok,
-                'SN' =>             ($barang_masuk->SN) ? $barang_masuk->SN : '-', #NI ngecek kalo ada SN ato ngga, kalo ngga ya diganti jadi -
+                'Processor' => ($barang_masuk->is_pc) ? $barang_masuk->Processor : '-',
+                'RAM' => ($barang_masuk->is_pc) ? $barang_masuk->RAM : '-',
+                'GPU' => ($barang_masuk->is_pc) ? $barang_masuk->GPU : '-',
+                'Storage' => ($barang_masuk->is_pc) ? $barang_masuk->Storage : '-',
+                'OS' => ($barang_masuk->is_pc) ? $barang_masuk->OS : '-',
+                'License' => ($barang_masuk->is_pc) ? $barang_masuk->License : '-',
+                'Monitor' => ($barang_masuk->is_pc) ? $barang_masuk->Monitor : '-',
+                'Keyboard' => ($barang_masuk->is_pc) ? $barang_masuk->Keyboard : '-',
+                'Mouse' => ($barang_masuk->is_pc) ? $barang_masuk->Mouse : '-',
+                'stok' => ($barang_masuk->is_pc) ? 1 : $request->stok,
+                'SN' => ($barang_masuk->SN) ? $barang_masuk->SN : '-', #NI ngecek kalo ada SN ato ngga, kalo ngga ya diganti jadi -
                 'keterangan' =>     $request->keterangan,
                 'gambar1' =>        $imageName1,
                 'gambar2' =>        $imageName2,
-                'assigned_user' =>  ($request->user) ? $request->user : '-',
-                'is_pc' =>          ($barang_masuk->is_pc) ? $barang_masuk->is_pc : false,
+                'assigned_user' => ($request->user) ? $request->user : '-',
+                'is_pc' => ($barang_masuk->is_pc) ? $barang_masuk->is_pc : false,
                 'created_at' =>     Carbon::now(),
             ]);
 
@@ -174,7 +174,7 @@ class BarangMasukController extends Controller
             $lokasi = Location::find($request->lokasi_id);
             $lokasi = $lokasi->lokasi;          ####
 
-            $model_data = CategoryStock::find( $barang_masuk->model_id);
+            $model_data = CategoryStock::find($barang_masuk->model_id);
 
             $kategori = Category::find($model_data->category_id);
             $kategori = $kategori->kategori;    #####
@@ -188,36 +188,32 @@ class BarangMasukController extends Controller
                 'status'    =>      $status,
                 'model'     =>      $model,
                 'kategori'  =>      $kategori,
-                'Processor' =>      ($barang_masuk->is_pc) ? $barang_masuk->Processor : '-',
-                'RAM' =>            ($barang_masuk->is_pc) ? $barang_masuk->RAM : '-',
-                'GPU' =>            ($barang_masuk->is_pc) ? $barang_masuk->GPU : '-',
-                'Storage' =>        ($barang_masuk->is_pc) ? $barang_masuk->Storage : '-',
-                'OS' =>             ($barang_masuk->is_pc) ? $barang_masuk->OS : '-',
-                'License' =>        ($barang_masuk->is_pc) ? $barang_masuk->License : '-',
-                'Monitor' =>        ($barang_masuk->is_pc) ? $barang_masuk->Monitor : '-',
-                'Keyboard' =>       ($barang_masuk->is_pc) ? $barang_masuk->Keyboard : '-',
-                'Mouse' =>          ($barang_masuk->is_pc) ? $barang_masuk->Mouse : '-',
-                'stok' =>           ($barang_masuk->is_pc) ? 1 : $request->stok,
-                'SN' =>             ($barang_masuk->SN) ? $barang_masuk->SN : '-',
+                'Processor' => ($barang_masuk->is_pc) ? $barang_masuk->Processor : '-',
+                'RAM' => ($barang_masuk->is_pc) ? $barang_masuk->RAM : '-',
+                'GPU' => ($barang_masuk->is_pc) ? $barang_masuk->GPU : '-',
+                'Storage' => ($barang_masuk->is_pc) ? $barang_masuk->Storage : '-',
+                'OS' => ($barang_masuk->is_pc) ? $barang_masuk->OS : '-',
+                'License' => ($barang_masuk->is_pc) ? $barang_masuk->License : '-',
+                'Monitor' => ($barang_masuk->is_pc) ? $barang_masuk->Monitor : '-',
+                'Keyboard' => ($barang_masuk->is_pc) ? $barang_masuk->Keyboard : '-',
+                'Mouse' => ($barang_masuk->is_pc) ? $barang_masuk->Mouse : '-',
+                'stok' => ($barang_masuk->is_pc) ? 1 : $request->stok,
+                'SN' => ($barang_masuk->SN) ? $barang_masuk->SN : '-',
                 'keterangan' =>     $request->keterangan,
                 'gambar1' =>        $imageName1,
                 'gambar2' =>        $imageName2,
-                'assigned_user' =>  ($request->user) ? $request->user : '-',
-                'is_pc' =>          ($barang_masuk->is_pc) ? $barang_masuk->is_pc : false,
+                'assigned_user' => ($request->user) ? $request->user : '-',
+                'is_pc' => ($barang_masuk->is_pc) ? $barang_masuk->is_pc : false,
                 'created_at' =>     Carbon::now(),
             ]);
 
-            // $Stock = CategoryStock::find($request->model_id);
-            // $newStock = $Stock->stok - ($request->is_pc) ? 1 : $request->stok;
-            // CategoryStock::where('model_id', $request->model_id)->update([
-            //     'stok' => $newStock,
-            // ]);
         }
 
-        return redirect()->back();
+        return redirect()->back()->with('sukses_notif', 'Data Berhasil Di Assign/Keluarkan');
     }
 
-    public function hapus_barang_masuk($id){
+    public function hapus_barang_masuk($id)
+    {
         // dd($id);
 
         $barang_masuk = BarangMasuk::find($id);
@@ -248,26 +244,26 @@ class BarangMasukController extends Controller
             'status'    =>      'DELETE',
             'model'     =>      $model,
             'kategori'  =>      $kategori,
-            'Processor' =>      ($barang_masuk->is_pc) ? $barang_masuk->Processor : '-',
-            'RAM' =>            ($barang_masuk->is_pc) ? $barang_masuk->RAM : '-',
-            'GPU' =>            ($barang_masuk->is_pc) ? $barang_masuk->GPU : '-',
-            'Storage' =>        ($barang_masuk->is_pc) ? $barang_masuk->Storage : '-',
-            'OS' =>             ($barang_masuk->is_pc) ? $barang_masuk->OS : '-',
-            'License' =>        ($barang_masuk->is_pc) ? $barang_masuk->License : '-',
-            'Monitor' =>        ($barang_masuk->is_pc) ? $barang_masuk->Monitor : '-',
-            'Keyboard' =>       ($barang_masuk->is_pc) ? $barang_masuk->Keyboard : '-',
-            'Mouse' =>          ($barang_masuk->is_pc) ? $barang_masuk->Mouse : '-',
-            'stok' =>           ($barang_masuk->is_pc) ? 1 : $barang_masuk->stok,
-            'SN' =>             ($barang_masuk->SN) ? $barang_masuk->SN : '-',
+            'Processor' => ($barang_masuk->is_pc) ? $barang_masuk->Processor : '-',
+            'RAM' => ($barang_masuk->is_pc) ? $barang_masuk->RAM : '-',
+            'GPU' => ($barang_masuk->is_pc) ? $barang_masuk->GPU : '-',
+            'Storage' => ($barang_masuk->is_pc) ? $barang_masuk->Storage : '-',
+            'OS' => ($barang_masuk->is_pc) ? $barang_masuk->OS : '-',
+            'License' => ($barang_masuk->is_pc) ? $barang_masuk->License : '-',
+            'Monitor' => ($barang_masuk->is_pc) ? $barang_masuk->Monitor : '-',
+            'Keyboard' => ($barang_masuk->is_pc) ? $barang_masuk->Keyboard : '-',
+            'Mouse' => ($barang_masuk->is_pc) ? $barang_masuk->Mouse : '-',
+            'stok' => ($barang_masuk->is_pc) ? 1 : $barang_masuk->stok,
+            'SN' => ($barang_masuk->SN) ? $barang_masuk->SN : '-',
             'keterangan' =>     $barang_masuk->keterangan,
             'gambar1' =>        $barang_masuk->gambar1,
             'gambar2' =>        $barang_masuk->gambar2,
             'assigned_user' =>  '-',
-            'is_pc' =>          ($barang_masuk->is_pc) ? $barang_masuk->is_pc : false,
+            'is_pc' => ($barang_masuk->is_pc) ? $barang_masuk->is_pc : false,
             'created_at' =>     Carbon::now(),
         ]);
 
-        return redirect()->back();
+        return redirect()->back()->with('sukses_notif', 'Data Berhasil Di Hapus');
     }
 
     public function view_detail_masuk($id)
@@ -276,5 +272,127 @@ class BarangMasukController extends Controller
         $detail = BarangMasuk::find($id);
         // dd($detail);
         return view('barangmasukdetail', compact('detail'));
+    }
+
+    public function update_barang_masuk($id)
+    {
+        $detail = BarangMasuk::find($id);
+        return view('updatebarangmasuk', compact('detail'));
+    }
+
+    public function do_update_barang_masuk(Request $request)
+    {
+
+        $data = BarangMasuk::find($request->masuk_id);
+        $data_gambar1 = $data->gambar1;
+        $data_gambar2 = $data->gambar2;
+
+        if ($request->file('gambar1')) {
+            // gambar pertama
+            $manager = new ImageManager(new Driver());
+            $imageName = time() . '_brg' . '.' . $request->file('gambar1')->getClientOriginalExtension();
+            $img = $manager->read($request->file('gambar1'));
+            $img = $img->resize(640, 360);
+            $img->toJpeg(70)->save(base_path('public\\storage\\images\\masukImage\\' . $imageName));
+            $imageName1 = 'images/masukImage/' . $imageName;
+
+            // BarangMasuk::where('masuk_id', $request->masuk_id)->update([
+            //     'gambar1' =>        $imageName1,
+            // ]);
+
+            $data_gambar1 = $imageName1;
+        }
+
+        if ($request->file('gambar2')) {
+            // gambar kedua
+            $manager2 = new ImageManager(new Driver());
+            $imageName = time() . '_rcpt' . '.' . $request->file('gambar2')->getClientOriginalExtension();
+            $img2 = $manager2->read($request->file('gambar2'));
+            $img2 = $img2->resize(640, 360);
+            $img2->toJpeg(70)->save(base_path('public\\storage\\images\\masukImage\\' . $imageName));
+            $imageName2 = 'images/masukImage/' . $imageName;
+
+            // BarangMasuk::where('masuk_id', $request->masuk_id)->update([
+            //     'gambar2' =>        $imageName2,
+            // ]);
+
+            $data_gambar2 = $imageName2;
+        }
+
+        BarangMasuk::where('masuk_id', $request->masuk_id)->update([
+            'Processor' =>      ($data->is_pc) ? $request->processor : '-',
+            'RAM' =>            ($data->is_pc) ? $request->ram : '-',
+            'GPU' =>            ($data->is_pc) ? $request->gpu : '-',
+            'Storage' =>        ($data->is_pc) ? $request->storage : '-',
+            'OS' =>             ($data->is_pc) ? $request->operating_system : '-',
+            'License' =>        ($data->is_pc) ? $request->license : '-',
+            'Monitor' =>        ($data->is_pc) ? $request->monitor : '-',
+            'Keyboard' =>       ($data->is_pc) ? $request->keyboard : '-',
+            'Mouse' =>          ($data->is_pc) ? $request->mouse : '-',
+            'SN' =>             $request->SN,
+            'stok'=>            ($data->is_pc) ? 1 : $request->stok, #NI ngecek kalo ada SN ato ngga, kalo ngga ya diganti jadi -
+            'gambar1' =>        $data_gambar1,
+            'gambar2' =>        $data_gambar2,
+            'keterangan' =>     $request->keterangan,
+            'updated_at' =>     Carbon::now(),
+        ]);
+
+        if(!$data->is_pc){
+            if($request->stok < $data->stok){
+                $Stock = CategoryStock::find($data->model_id);
+                $minus = $data->stok - $request->stok;
+                $newStock = $Stock->stok - $minus;
+            }
+            if($request->stok > $data->stok){
+                $Stock = CategoryStock::find($data->model_id);
+                $added = $request->stok - $data->stok;
+                $newStock = $Stock->stok + $added;
+            }
+        }
+
+        CategoryStock::where('model_id', $data->model_id)->update([
+            'stok' => $newStock,
+        ]);
+
+        // buat history update ////////////////////////////////////////////////////////////
+        $barang_masuk = BarangMasuk::find($request->masuk_id);
+        $kategoriStok = CategoryStock::find($barang_masuk->model_id);
+
+        // ini buat history
+        $lokasi = Location::find($barang_masuk->location_id);
+        $lokasi = $lokasi->lokasi;          ####
+
+        $kategori = Category::find($kategoriStok->category_id);
+        $kategori = $kategori->kategori;    #####
+
+        $model = $kategoriStok->model_name;   #####
+        History::insert([
+            'barang_id' =>      $barang_masuk->masuk_id,
+            'barang'    =>      'MASUK',
+            'lokasi'    =>      $lokasi,
+            'by_user'   =>      auth()->user()->name, #### ganti ke auth,current user
+            'status'    =>      'UPDATE',
+            'model'     =>      $model,
+            'kategori'  =>      $kategori,
+            'Processor' =>      ($barang_masuk->is_pc) ? $request->processor : '-',
+            'RAM' =>            ($barang_masuk->is_pc) ? $request->ram : '-',
+            'GPU' =>            ($barang_masuk->is_pc) ? $request->gpu : '-',
+            'Storage' =>        ($barang_masuk->is_pc) ? $request->storage : '-',
+            'OS' =>             ($barang_masuk->is_pc) ? $request->operating_system : '-',
+            'License' =>        ($barang_masuk->is_pc) ? $request->license : '-',
+            'Monitor' =>        ($barang_masuk->is_pc) ? $request->monitor : '-',
+            'Keyboard' =>       ($barang_masuk->is_pc) ? $request->keyboard : '-',
+            'Mouse' =>          ($barang_masuk->is_pc) ? $request->mouse : '-',
+            'stok' =>           ($barang_masuk->is_pc) ? 1 : $request->stok, ### REQUEST STOK
+            'SN' =>             $request->SN,
+            'keterangan' =>     $request->keterangan,
+            'gambar1' =>        $data_gambar1,
+            'gambar2' =>        $data_gambar2,
+            'assigned_user' =>  '-',
+            'is_pc' =>          ($barang_masuk->is_pc) ? $barang_masuk->is_pc : false,
+            'created_at' =>     Carbon::now(),
+        ]);
+
+        return redirect()->back()->with('sukses_notif', 'Data Berhasil Di Ubah');
     }
 }
