490d8b2767e23a6d08cc5121236fbeabb4754aa5
FrL1902 <farrelrasyad.binus.ac.id>
2025-02-23 22:59:41 +0700

    about page updated with links

--- truckingSystem/resources/views/about.blade.php
+++ truckingSystem/resources/views/about.blade.php
@@ -23,6 +23,15 @@
         <div class="card">
             <div class="card-header">
                 <h1>Hey</h1>
+                <p>Please refer to the documentations here</p>
+                <br>
+                <a href="https://drive.google.com/file/d/1TWbi6QvsHg4XByjyy50Wp3c48EOdbm1o/view?usp=sharing"  target="_blank">My personal CV</a>
+                <br>
+                <a href="https://drive.google.com/file/d/12aHB56rO_0M1-fg5Y-KY49EHgGCSKdA-/view?usp=sharing"  target="_blank">The Proposal</a>
+                <br>
+                <a href="https://drive.google.com/file/d/1zJr1HrsgKVPvW654tFgUFdST30_i5ACm/view?usp=sharing"  target="_blank">System Planning and Design</a>
+                <br>
+                <a href="https://github.com/FrL1902/trucking-system"  target="_blank">The GitHub Repository for this project</a>
             </div>
         </div>
     </div>
